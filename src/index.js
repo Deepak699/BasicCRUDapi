9149b1c9c907b1fe925a23eb42bbd8fb466432db
Deepak699 <48008609+Deepak699@users.noreply.github.com>
2021-07-22 10:52:13 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
+//Importing Modules
 const express = require("express");
 const userRouter = require("../routes/Users");
 require("dotenv").config();
 require("../db/db");
+
+//Initializing Server
 const app = express();
-app.use(express.json())
+
+//Configuring Server to recieve data in json format
+app.use(express.json());
+
+//Using userRouter
 app.use(userRouter);
+
 app.listen("3000", () => {
   console.log("Listiening");
 });
